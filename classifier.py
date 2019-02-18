00ee7a2de8541a23ed1e24490c59694a556261e7
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-02-17 23:58:59 -0500

    Working model gen and accuracy

--- classifier.py
+++ classifier.py
@@ -1,5 +1,6 @@
 import pickle
 import sys
+from operator import add
 
 
 def classify(vector, weights):
@@ -9,15 +10,32 @@ def classify(vector, weights):
     return 1.0 if activator_sum >= 0.0 else 0.0
 
 
-def train_model(data_set, epoch_n):
-    weights = [0.0 for i in range(len(data_set[0][0]))]
+def calc_accuracy(data_set, weights):
+    correct = 1.0
+    for vector in data_set:
+        guess = classify(vector[0], weights)
+        if vector[1] == guess:
+            correct += 1.0
+    return correct / len(data_set) * 100.0
+
+
+def train(data_set, epoch_n, learn_rate=1.0):
+    t_weights = [0.0 for i in range(len(data_set[0][0]))]
+    weights = t_weights.copy()
     for epoch in range(epoch_n):
         for vector in data_set:
             classification = classify(vector[0], weights)
-    return weights
+            error = vector[1] - classification
+            for j in range(len(weights)):
+                weights[j] += learn_rate * error * vector[0][j]
+            t_weights = list(map(add, weights, t_weights))
+        w_acc = calc_accuracy(data_set, weights)
+        print(w_acc)
+
+    return weights, t_weights
 
 
-num_epoch = sys.argv[1]
+num_epoch = int(sys.argv[1])
 
 training_set = []
 testing_set = []
@@ -28,5 +46,5 @@ with open('train_data_dump', 'rb') as f:
 with open('test_data_dump', 'rb') as f:
     testing_set = pickle.load(f)
 
-model_weights = [0.0 for i in range(len(testing_set[0][0]))]
-avg_weights = model_weights.copy()
+model, avg_model = train(training_set,  num_epoch)
+print(model)
