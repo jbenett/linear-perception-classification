dcf7a019ce8c0bc695e137fe63b18ac531c55998
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-02-18 17:43:03 -0500

    working model generation

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 import pickle
-import sys
 from operator import add
+from numpy import *
+from matplotlib.pyplot import *
 
 
 def classify(vector, weights):
@@ -16,29 +17,36 @@ def calc_accuracy(data_set, weights):
         guess = classify(vector[0], weights)
         if vector[1] == guess:
             correct += 1.0
-    return correct / len(data_set) * 100.0
+    return round(correct / len(data_set) * 100.0, 5)
 
 
 def train(data_set, epoch_n, learn_rate=1.0):
     t_weights = [0.0 for i in range(len(data_set[0][0]))]
     weights = t_weights.copy()
+    history = []
+    averaged_history = []
     for epoch in range(epoch_n):
+        average = []
         for vector in data_set:
             classification = classify(vector[0], weights)
             error = vector[1] - classification
             for j in range(len(weights)):
                 weights[j] += learn_rate * error * vector[0][j]
             t_weights = list(map(add, weights, t_weights))
-        w_acc = calc_accuracy(data_set, weights)
-        print(w_acc)
-
-    return weights, t_weights
+        history.append(weights.copy())
+        for i in range(len(t_weights)):
+            average.append((1 / (epoch_n * len(data_set))) * t_weights[i])
+        averaged_history.append(average.copy())
+    return history, averaged_history
 
 
 num_epoch = int(sys.argv[1])
 
-training_set = []
-testing_set = []
+m_train_acc = []
+t_train_acc = []
+
+m_test_acc = []
+t_test_acc = []
 
 with open('train_data_dump', 'rb') as f:
     training_set = pickle.load(f)
@@ -46,5 +54,16 @@ with open('train_data_dump', 'rb') as f:
 with open('test_data_dump', 'rb') as f:
     testing_set = pickle.load(f)
 
-model, avg_model = train(training_set,  num_epoch)
-print(model)
+model_history, avg_model_history = train(training_set,  num_epoch)
+
+for n in range(num_epoch):
+    m_train_acc.append(calc_accuracy(training_set, model_history[n]))
+    t_train_acc.append(calc_accuracy(training_set, avg_model_history[n]))
+    m_test_acc.append(calc_accuracy(testing_set, model_history[n]))
+    t_test_acc.append(calc_accuracy(testing_set, avg_model_history[n]))
+
+print(m_train_acc)
+print(t_train_acc)
+
+print(m_test_acc)
+print(t_test_acc)
